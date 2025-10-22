538c40cb7fb7aaba3f3ddf457a31defa8ff60dfa
Diko20 <00001122543645SP@al.educacao.sp.gov.br>
2025-10-22 08:02:48 -0300

    Create and modify a matrix of names
    Initialize a matrix with names and update elements.

---
+++ Matriz.py
@@ -0,0 +1,15 @@
+matriz = [
+    ["Andrew", "Thay", "Enzo"],
+    ["Evelyn", "Rodrigo", "Matheus"]
+    ]
+
+print(matriz[0][0])
+
+matriz[0][0] = "Gabriel"
+print("Novo nome: ", matriz)
+
+matriz[0][1] = "Yamil"
+print("Novo nome: ", matriz)
+
+matriz[0][2] = "Diego"
+print("Novo nome: ", matriz)
