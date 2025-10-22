9a315d7ca85db303300e0b17344876a46f79c882
Diko20 <00001122543645SP@al.educacao.sp.gov.br>
2025-10-22 08:59:49 -0300

    Add files via upload

---
+++ filmes.py
@@ -0,0 +1,21 @@
+matriz = [
+    ["Homem Aranha", "Sonic 3", "Quarteto Fantástico"],
+    ["Superman", "Jumanji", "Vingadores: Ultimato"],
+    ["Clube da luta", "It: A coisa", "Batman: O Cavaleiro das Trevas"],
+    ["O Poderoso Chefão", "O Senhor dos Anéis", "Avatar"],
+    ["Sexta feira 13", "Gente Grande", "Meu Malvado Favorito"]
+    ]
+
+print(matriz[0][0])
+
+matriz[0][0] = "Rio"
+print("Novo nome: ", matriz)
+
+matriz[0][1] = "Demon Slayer: Castelo Infinito"
+print("Novo nome: ", matriz)
+
+matriz[0][2] = "Homem-Aranha: Através do Aranhaverso"
+print("Novo nome: ", matriz)
+
+matriz[1][0] = "Pânico"
+print("Novo nome: ", matriz)
